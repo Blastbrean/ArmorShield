0a713008defcf174aca41a4b898027de8dbe1dae
Blastbrean <blastbrean@waifu.club>
2024-11-02 11:42:20 -0700

    Logging

--- loader.go
+++ loader.go
@@ -112,7 +112,7 @@ func (ls *loaderServer) readPump(ctx context.Context, cl *client, c *websocket.C
 			return tracerr.Wrap(err)
 		}
 
-		cl.logger.Warn("unmarshal packet", slog.Any("dba", len(dba)), slog.Any("stringLen", len(b.String())))
+		cl.logger.Warn("unmarshal packet", slog.Any("dba", dba), slog.Any("stringLen", len(b.String())))
 
 		var pk Packet
 		err = msgpack.Unmarshal(dba, &pk)
