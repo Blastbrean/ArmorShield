c790bd3dd7a235fc11cde1433cac95cf39a490e0
Blastbrean <blastbrean@waifu.club>
2024-11-02 22:31:15 -0700

    Proper length check

--- report.go
+++ report.go
@@ -107,7 +107,7 @@ type reportHandler struct {
 
 // Process function check data
 func (sh reportHandler) processFunctionCheckData(cl *client, fd *functionData, lfcd []FunctionCheckData, identifier string) bool {
-	if len(lfcd) != (CheckFunctionRestore + 1) {
+	if len(lfcd) != CheckFunctionRestore {
 		cl.logger.Warn("invalid function check data length", slog.String("identifier", identifier), slog.Int("length", len(lfcd)))
 		return false
 	}
